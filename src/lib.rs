0667400a8af1a0b84a612bacfce2c3e73d0c73f3
Neil LoBracco <neil@daft.io>
2015-04-19 10:43:16 -0400

    Enhance support for responding to webhooks.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub use self::hyper::method::Method::{Post,Get,Put};
 pub use message::{Message,OutboundMessage};
 pub use call::{Call,OutboundCall};
 use std::collections::HashMap;
+use std::io::Write;
 
 pub struct Client {
     account_id : String,
@@ -88,4 +89,28 @@ impl Client {
         };
         Ok(decoded)
     }
+
+    pub fn respond_to_webhook<T:FromMap,F>(&self, mut req: hyper::server::Request, mut res: hyper::server::Response, mut logic:  F)
+    where F: FnMut(T) -> twiml::Twiml {
+        let o: T = match self.parse_request::<T>(&mut req){
+            Err(e) => {
+                println!("{:?}",e);
+                let mut res = res.start().unwrap();
+                res.write_all("Error.".as_bytes()).unwrap();
+                res.end().unwrap();
+                return
+            },
+                Ok(obj) => obj,
+        };
+        let t = logic(o);
+        let body = t.as_twiml();
+        let mime: mime::Mime = "text/xml".parse().unwrap();
+        let content_type_header = hyper::header::ContentType(mime);
+        res.headers_mut().set(content_type_header);
+        res.headers_mut().set(hyper::header::ContentLength(body.len() as u64));
+        let mut res = res.start().unwrap();
+        res.write_all(body.as_bytes()).unwrap();
+        res.end().unwrap();
+    }
+
 }
--- src/twiml.rs
+++ src/twiml.rs
@@ -1,28 +1,75 @@
-pub enum Action {
-    Message(String),
-    Redirect(String),
-}
-
-impl Action {
-    fn as_xml(&self) -> String {
-        let (tag_name, inner) = match *self {
-            Action::Message(ref s) => ("Message", s),
-            Action::Redirect(ref s) => ("Redirect",s),
-        };
-        format!("<{}>{}</{}>",tag_name,inner,tag_name)
-    }
+pub trait Action {
+    fn as_twiml(&self) -> String;
 }
 
 pub struct Twiml {
-    actions : Vec<Action>,
+    body : String,
 }
 
 impl Twiml {
     pub fn new() -> Twiml {
-        Twiml { actions : vec![] }
+        Twiml { body : "".to_string() }
     }
-    pub fn add(&mut self, a: Action) -> &Self {
-        self.actions.push(a);
+    pub fn add(&mut self, a: &Action) -> &Self {
+        let twiml = a.as_twiml();
+        self.body.push_str((&twiml as &AsRef<str>).as_ref());
         self
+    }
+    pub fn as_twiml(&self) -> String {
+        let b: &str = self.body.as_ref();
+        format!("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response>{}</Response>",b)
+    }
+}
+
+fn format_xml_string(tag: &str, attributes: &[(&str,&str)], inner: &str) -> String {
+    let attribute_string = match attributes.len(){
+        0 => "".to_string(),
+        _ => attributes.iter().map(|t|{
+            format!("{}=\"{}\"",t.0,t.1)
+        }).fold("".to_string(),|mut acc,v|{
+            acc.push_str(" ");
+            acc.push_str(&v);
+            acc
+        })
+    };
+    let attribute_str: &str = attribute_string.as_ref();
+    format!("<{}{}>{}</{}>",tag,attribute_str,inner,tag)
+}
+
+pub struct Message {
+    txt : String,
+}
+impl Action for Message {
+    fn as_twiml(&self) -> String {
+        format_xml_string("Message",&vec![],&self.txt)
+    }
+}
+impl Message {
+    pub fn new(txt: String) -> Message {
+        Message { txt: txt }
+    }
+}
+
+pub enum Method {
+    Get,
+    Post,
+}
+
+pub struct Redirect {
+    url : String,
+    method : Method,
+}
+impl Action for Redirect {
+    fn as_twiml(&self) -> String {
+        let method_str = match self.method {
+            Method::Get => "GET",
+            Method::Post => "POST",
+        };
+        format_xml_string("Redirect",&vec![("method",method_str)],&self.url)
+    }
+}
+impl Redirect {
+    pub fn new(url: String, method: Method) -> Redirect {
+        Redirect { url: url, method: method }
     }
 }
