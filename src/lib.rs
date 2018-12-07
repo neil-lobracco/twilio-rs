262e7d21366ff2e53f39f9f48160c7fbb34d06ea
Mike Berry <mikethetike@tari.com>
2018-12-07 13:30:33 +0200

    Add better bad request handling

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,35 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+
 mod message;
 mod call;
 mod webhook;
 pub mod twiml;
+
 use std::io::Read;
-use self::hyper::header::{Authorization,Basic};
+use self::hyper::header::{Authorization, Basic};
 use hyper::net::HttpsConnector;
 use self::hyper::status::StatusCode;
-pub use self::hyper::method::Method::{Post,Get,Put};
+pub use self::hyper::method::Method::{Post, Get, Put};
 use hyper_native_tls::NativeTlsClient;
-pub use message::{Message,OutboundMessage};
-pub use call::{Call,OutboundCall};
+pub use message::{Message, OutboundMessage};
+pub use call::{Call, OutboundCall};
 use std::collections::HashMap;
 use std::io::Write;
 use std::error::Error;
 use std::fmt;
 
 pub struct Client {
-    account_id : String,
-    auth_token : String,
-    auth_header : Authorization<Basic>,
+    account_id: String,
+    auth_token: String,
+    auth_header: Authorization<Basic>,
 }
 
-fn url_encode(params: &[(&str,&str)]) -> String {
+fn url_encode(params: &[(&str, &str)]) -> String {
     params.iter().map(|&t| {
-        let (k,v) = t;
-        format!("{}={}",k,v)
+        let (k, v) = t;
+        format!("{}={}", k, v)
     }).fold("".to_string(), |mut acc, item| {
         acc.push_str(&item);
         acc.push_str("&");
@@ -40,16 +42,17 @@ fn url_encode(params: &[(&str,&str)]) -> String {
 }
 
 fn basic_auth_header(username: String, password: String) -> Authorization<Basic> {
-    Authorization(Basic { username : username, password : Some(password) })
+    Authorization(Basic { username: username, password: Some(password) })
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub enum TwilioError {
     NetworkError,
     HTTPError,
     ParsingError,
     AuthError,
     BadRequest,
+    ValidationError { code: u64, message: String, more_info: String, status: u64 },
 }
 
 impl fmt::Display for TwilioError {
@@ -72,27 +75,34 @@ impl Error for TwilioError {
             ParsingError => "ParsingError",
             AuthError => "AuthError",
             BadRequest => "BadRequest",
+            ValidationError{code, message, more_info, status} => "Validation Error"
         }
     }
 }
 
+impl From<serde_json::Error> for TwilioError{
+    fn from(e: serde_json::Error) -> Self {
+       TwilioError::ParsingError
+    }
+}
+
 pub trait FromMap {
-    fn from_map(&HashMap<&str,&str>) -> Result<Box<Self>,TwilioError>;
+    fn from_map(&HashMap<&str, &str>) -> Result<Box<Self>, TwilioError>;
 }
 
 impl Client {
     pub fn new(account_id: &str, auth_token: &str) -> Client {
         Client {
-            account_id : account_id.to_string(),
-            auth_token : auth_token.to_string(),
-            auth_header : basic_auth_header(account_id.to_string(),auth_token.to_string()),
+            account_id: account_id.to_string(),
+            auth_token: auth_token.to_string(),
+            auth_header: basic_auth_header(account_id.to_string(), auth_token.to_string()),
         }
     }
 
-    fn send_request<T>(&self, method: hyper::method::Method, endpoint: &str, params: &[(&str,&str)]) -> Result<T, TwilioError>
+    fn send_request<T>(&self, method: hyper::method::Method, endpoint: &str, params: &[(&str, &str)]) -> Result<T, TwilioError>
         where T: serde::de::DeserializeOwned,
     {
-        let url = format!("https://api.twilio.com/2010-04-01/Accounts/{}/{}.json",self.account_id,endpoint);
+        let url = format!("https://api.twilio.com/2010-04-01/Accounts/{}/{}.json", self.account_id, endpoint);
         let ssl = NativeTlsClient::new().unwrap();
         let connector = HttpsConnector::new(ssl);
         let mut http_client = hyper::Client::with_connector(connector);
@@ -100,7 +110,7 @@ impl Client {
         let mime: mime::Mime = "application/x-www-form-urlencoded".parse().unwrap();
         let content_type_header = hyper::header::ContentType(mime);
         let req = http_client
-            .request(method,&*url)
+            .request(method, &*url)
             .body(post_body)
             .header(self.auth_header.clone())
             .header(content_type_header);
@@ -115,28 +125,32 @@ impl Client {
         };
         match resp.status {
             StatusCode::Created | StatusCode::Ok => (),
+            StatusCode::BadRequest => {
+                let val: serde_json::Value = serde_json::from_str(&body_str)?;
+                return Err(TwilioError::ValidationError { code: val["code"].as_u64().unwrap_or_default(), message: val["message"].as_str().unwrap_or("").to_string(), more_info: val["more_info"].as_str().unwrap_or("").to_string(), status: val["status"].as_u64().unwrap_or_default() });
+            }
             _ => {
-                return Err(TwilioError::HTTPError)
+                return Err(TwilioError::HTTPError);
             }
         };
         let decoded: T = match serde_json::from_str(&body_str) {
             Ok(obj) => obj,
-            Err(_)  => return Err(TwilioError::ParsingError),
+            Err(_) => return Err(TwilioError::ParsingError),
         };
         Ok(decoded)
     }
 
-    pub fn respond_to_webhook<T:FromMap,F>(&self, req: &mut hyper::server::Request, mut res: hyper::server::Response, mut logic:  F)
-    where F: FnMut(T) -> twiml::Twiml {
-        let o: T = match self.parse_request::<T>(req){
+    pub fn respond_to_webhook<T: FromMap, F>(&self, req: &mut hyper::server::Request, mut res: hyper::server::Response, mut logic: F)
+        where F: FnMut(T) -> twiml::Twiml {
+        let o: T = match self.parse_request::<T>(req) {
             Err(_) => {
                 *res.status_mut() = hyper::BadRequest;
                 let mut res = res.start().unwrap();
                 res.write_all("Error.".as_bytes()).unwrap();
                 res.end().unwrap();
-                return
-            },
-                Ok(obj) => *obj,
+                return;
+            }
+            Ok(obj) => *obj,
         };
         let t = logic(o);
         let body = t.as_twiml();
@@ -148,5 +162,4 @@ impl Client {
         res.write_all(body.as_bytes()).unwrap();
         res.end().unwrap();
     }
-
 }
